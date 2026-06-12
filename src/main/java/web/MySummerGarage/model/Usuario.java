f0457b7f60f3f5503aa29f9eba094c1118601a2e
BrunoLoby <bruno.lobianco19@gmail.com>
2026-06-12 03:24:04 -0300

    updates

--- src/main/java/web/MySummerGarage/model/Usuario.java
+++ src/main/java/web/MySummerGarage/model/Usuario.java
@@ -1,45 +1,88 @@
 package web.MySummerGarage.model;
 
-public class Usuario {
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
 
-    public int id;
-    public String nome;
-    public String email;
-    public String senha;
+@Entity
+@Table(name = "usuario")
+public class Usuario implements Serializable {
 
-    //GETTERS E SETTERS
-    public int getId() {
-        return id;
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @SequenceGenerator(name = "geradorUsuario", sequenceName = "usuario_codigo_seq", allocationSize = 1)
+    @GeneratedValue(generator = "geradorUsuario", strategy = GenerationType.SEQUENCE)
+    private Long codigo;
+
+    private String nome;
+    private String email;
+    private String senha;
+
+    @Column(name = "nome_usuario")
+    private String nomeUsuario;
+
+    private boolean ativo;
+
+    @ManyToMany
+    @JoinTable(
+        name = "usuario_perfil",
+        joinColumns = @JoinColumn(name = "codigo_usuario"),
+        inverseJoinColumns = @JoinColumn(name = "codigo_perfil")
+    )
+    private List<Perfil> perfis = new ArrayList<>();
+
+    public Long getCodigo() { return codigo; }
+    public void setCodigo(Long codigo) { this.codigo = codigo; }
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    public String getSenha() { return senha; }
+    public void setSenha(String senha) { this.senha = senha; }
+    public String getNomeUsuario() { return nomeUsuario; }
+    public void setNomeUsuario(String nomeUsuario) { this.nomeUsuario = nomeUsuario; }
+    public boolean isAtivo() { return ativo; }
+    public void setAtivo(boolean ativo) { this.ativo = ativo; }
+    public List<Perfil> getPerfis() { return perfis; }
+    public void setPerfis(List<Perfil> perfis) { this.perfis = perfis; }
+
+    public void adicionarPerfil(Perfil perfil) { perfis.add(perfil); }
+    public void removerPerfil(Perfil perfil) { perfis.remove(perfil); }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+        return result;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (getClass() != obj.getClass()) return false;
+        Usuario other = (Usuario) obj;
+        if (codigo == null) {
+            if (other.codigo != null) return false;
+        } else if (!codigo.equals(other.codigo)) return false;
+        return true;
     }
 
-    public String getNome() {
-        return nome;
+    @Override
+    public String toString() {
+        return "Usuario [codigo=" + codigo + ", nome=" + nome + ", email=" + email
+                + ", nomeUsuario=" + nomeUsuario + ", ativo=" + ativo + "]";
     }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
-
-    
-
-}
+}   
